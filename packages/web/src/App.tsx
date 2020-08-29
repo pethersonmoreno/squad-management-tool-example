a281260bd4cb5f4accd7c999674004d703c61b6d
Petherson Moreno <pethermoreno@gmail.com>
2020-08-29 19:26:22 -0300

    feat: added routes to web project

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <Link to="/create-team">Create team</Link>
+    </div>
+  );
+}
+
+function TeamForm() {
+  return (
+    <div>
+      <h2>Team Form</h2>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/create-team">
+          <TeamForm />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
