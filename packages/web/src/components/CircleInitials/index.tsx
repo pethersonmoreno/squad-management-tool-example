158098fe6b7dfd10f6be9c830f4d137ed18944ab
Petherson Moreno <pethermoreno@gmail.com>
2020-08-30 13:34:35 -0300

    feat: created circle initials and header

--- packages/web/src/components/CircleInitials/index.tsx
+++ packages/web/src/components/CircleInitials/index.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './CircleInitials.scss';
 
-function CircleInitials() {
+type TCircleInitialsProps = {
+  size: number;
+  name: string;
+}
+
+const getInitialsFromName = (name: string): string => {
+  let result = name;
+  const regex = /([a-zA-Z])[a-zA-Z]+/;
+  while(regex.test(result)){
+    result = result.replace(regex, '$1');
+  }
+  result = result.replace(/\s/, '');
+  return result;
+}
+
+const getInitialsFirstLastFromName = (name: string): string | null => {
+  
+  const allInitials = getInitialsFromName(name);
+  console.log('name: ', name);
+  console.log('allInitials: ', allInitials);
+  if(allInitials.length === 1){
+    return allInitials;
+  }
+  if(allInitials.length > 1){
+    return `${allInitials.substr(0, 1)}${allInitials.substr(allInitials.length - 1, 1)}`;
+  }
+  return null;
+};
+const INITIAL_IF_NOT_FOUND = 'U';
+
+function CircleInitials({
+  size,
+  name,
+}: TCircleInitialsProps) {
+  const initials = useMemo(()=> getInitialsFirstLastFromName(name) || INITIAL_IF_NOT_FOUND, [name]);
   return (
-    <div>
+    <div className="circle-initials" style={{fontSize: size}}>
+      <svg viewBox="0 0 35 35" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="17.5" cy="17.5" r="17.5" />
+      </svg>
+      <div className={'initials'}>{initials}</div>
     </div>
   );
 }
--- packages/web/src/components/Header/index.tsx
+++ packages/web/src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Logo from '../../assets/images/logo.svg';
 import './Header.scss';
+import CircleInitials from '../CircleInitials';
 
 function Header() {
+  const userName = 'John Doe';
   return (
     <header className="app-header">
-      <img className="logo" src={Logo} alt="Logo" />
-      <p className="app-name">Squad Management Tool</p>
+      <div className="__content">
+        <img className="__logo" src={Logo} alt="Logo" />
+        <p className="__app-name">Squad Management Tool</p>
+        <div className="__user-info">
+          <p className="__user-name">{userName}</p>
+          <CircleInitials size={35} name={userName} />
+        </div>
+      </div>
     </header>
   );
 }
